08ab52199f2ba517194029785a2a754979c39b90
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-07-23 23:57:29 +0300

    добавила решение 8й задачи, внесла некоторые изменения в стили

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ import galleryItems from './app.js';
 
 const refs = {
     gallery: document.querySelector('.js-gallery'),
-    lightbox: document.querySelector('.js-lightbox'),
+    backdrop: document.querySelector('.js-lightbox'),
     overlay: document.querySelector('.lightbox__overlay'),
-    content: document.querySelector('.lightbox__content'),
+    modal: document.querySelector('.lightbox__content'),
     image: document.querySelector('.lightbox__image'),
-    closeBtn: document.querySelector('.lightbox__button'),
+    closeBtn: document.querySelector('[data-action="close-lightbox"]'),
 };
 
 function creatGalleryElement() {
@@ -23,7 +23,6 @@ function creatGalleryElement() {
                 src="${preview}"
                 data-source="${original}"
                 alt="${description}"
-                data-index="${index}"
             />
         </a>
     </li>`})
@@ -49,5 +48,43 @@ refs.gallery.insertAdjacentHTML('beforeend', creatGalleryElement());
 // const galleryMarkUp = galleryItems.reduce(creatGalleryElement, '');
 // refs.gallery.insertAdjacentHTML('beforeend', galleryMarkUp);
 
+refs.gallery.addEventListener('click', onOpenModal);
+refs.closeBtn.addEventListener('click', onCloseModal);
+refs.overlay.addEventListener('click', onDivOverlayClick);
 
+function onOpenModal(event) {
+    window.addEventListener('keydown', onEscKeyPress);
+    event.preventDefault();
+    const target = event.target;
+    if (target.nodeName !== 'IMG') {
+        return;
+    } if (target.nodeName === 'IMG') {
+            refs.backdrop.classList.add('is-open');
+            refs.image.src = target.dataset.source;
+            refs.image.alt = target.alt;
+        }
+};
 
+function onCloseModal(event) {
+    window.removeEventListener('keydown', onEscKeyPress);
+
+    refs.backdrop.classList.remove('is-open');
+    refs.image.src = '';
+    refs.image.alt = '';
+};
+
+function onDivOverlayClick(event) {
+    // console.log(event.currentTarget);
+    // console.log(event.target);
+    if (event.currentTarget === event.target) {
+        // console.log('кликнули в оверлэй');
+        onCloseModal();
+    }
+};
+
+function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    if (event.code === ESC_KEY_CODE) {
+        onCloseModal();
+    }
+};
